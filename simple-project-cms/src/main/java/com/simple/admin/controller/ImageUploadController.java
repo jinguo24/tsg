021f755f456e1af7f7c38e8fc07190cbaf8a015b
zhengfy1 <zhengfy1@lenovo.com>
2016-11-09 15:58:46 +0800

    update

--- simple-project-cms/src/main/java/com/simple/admin/controller/ImageUploadController.java
+++ simple-project-cms/src/main/java/com/simple/admin/controller/ImageUploadController.java
@@ -19,12 +19,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.simple.common.config.EnvPropertiesConfiger;
 import com.simple.common.image.Thumbnailator;
 import com.simple.common.util.AjaxWebUtil;
 import com.simple.common.util.DateUtil;
 import com.simple.common.util.FileUploadUtil;
 import com.simple.common.util.ImageHandleUtil;
 import com.simple.common.util.PrimaryKeyUtil;
+import com.simple.fileencrypt.FileEncryptUtil;
 @Controller
 @RequestMapping(value = "/image")
 public class ImageUploadController {
@@ -33,26 +35,21 @@ public class ImageUploadController {
 
 	@RequestMapping(value="/uploadFile",method = RequestMethod.POST)
 	@ResponseBody
-	public String upload(@RequestParam("file") MultipartFile[] files, HttpServletRequest request, HttpServletResponse response) throws Exception {
-		System.out.println(">>>>"+DateUtil.date2AllString(new Date()));
+	public String upload(@RequestParam("file") MultipartFile[] files,int widht,int height, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		try {
 			List<String> images = new ArrayList<String>();
 			for (MultipartFile file:files ) {
 				long timestart = System.currentTimeMillis();
-//				String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
-//				if (StringUtils.isEmpty(suffix)) {
-//					suffix = request.getParameter("suffix");
-//				}
-//				suffix="jpg";
 				String imagewidth = request.getParameter("width");
 				String foler = FileUploadUtil.UPLOAD_IMAGE_DIR +System.currentTimeMillis();
-				File folerfile = new File(foler);
+				File folerfile = new File(EnvPropertiesConfiger.getValue("uploadDir")+foler);
 				if (!folerfile.exists()) {
 					folerfile.mkdirs();
 				}
 				String filepath = foler+ File.separator + PrimaryKeyUtil.getUUID() ;
 				String path = filepath+".jpg";
-				Thumbnailator.scale(file.getInputStream(), 1920, 1080, 0.4f, 0, filepath+".jpg");
+				Thumbnailator.scale(file.getInputStream(), widht, height, 1.0f, 0, filepath+".jpg");
+				/**不需要裁剪正方形
 				long time1 = System.currentTimeMillis();
 				System.out.println(">>>>img time1 : "+(time1-timestart));
 				if (null != imagewidth) {
@@ -67,29 +64,31 @@ public class ImageUploadController {
 					}
 				}
 				System.out.println(">>>>img time2 : "+(System.currentTimeMillis()-time1));
+				*/
 				images.add(path);
-				
-//				File f = new File(foler+ File.separator + PrimaryKeyUtil.getUUID() + "." + suffix);
-//				file.transferTo(f);//此处会生成图片到磁盘
-//				//先上传一张背景为黑色的图片
-//				String blackFile = ImageHandleUtil.img_change(f,null, false, false);
-//				//File f = FileUploadUtil.getFileByInputStream(file.getInputStream(), suffix, FileUploadUtil.UPLOAD_IMAGE_DIR);
-//				String path = blackFile.replace(EnvPropertiesConfiger.getValue("uploadDir"), "");
-//				if ( null != imagewidth) {
-//					int width = 0;
-//					try {
-//						width = Integer.parseInt(imagewidth);
-//					}catch(Exception e) {
-//					}
-//					if (width>0) {
-//						//再上传一张背景为白色的图片
-//						//String whiteFile = ImageHandleUtil.img_change(f,"w", false, true);
-//						ImageHandleUtil.cutImage(new File(blackFile), width);
-//						path = ImageHandleUtil.getScaleFilePath(blackFile.replace(EnvPropertiesConfiger.getValue("uploadDir"), ""),width);
-//					}
-//				}
-//				images.add(path);
 			}
+			return AjaxWebUtil.sendAjaxResponse(request, response, true,"上传成功", images);
+		}catch(Exception e) {
+			log.error("上传失败",e);
+			return AjaxWebUtil.sendAjaxResponse(request, response, false,"上传失败:"+e.getLocalizedMessage(), e.getLocalizedMessage());
+		}
+	}
+	
+	@RequestMapping(value="/uploadEncrypt",method = RequestMethod.POST)
+	@ResponseBody
+	public String uploadEncrypt(@RequestParam("file") MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		try {
+			List<String> images = new ArrayList<String>();
+			String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf(".")+1);
+			String foler = FileUploadUtil.UPLOAD_ENCRYPT_DIR +System.currentTimeMillis();
+			File folerfile = new File(EnvPropertiesConfiger.getValue("uploadDir")+foler);
+			if (!folerfile.exists()) {
+				folerfile.mkdirs();
+			}
+			String filepath = foler+ File.separator + PrimaryKeyUtil.getUUID() ;
+			String path = filepath+"."+suffix;
+			new FileEncryptUtil("simplefileencryptkey").encrypt(file.getInputStream(), path);
+			images.add(path);
 			return AjaxWebUtil.sendAjaxResponse(request, response, true,"上传成功", images);
 		}catch(Exception e) {
 			log.error("上传失败",e);
